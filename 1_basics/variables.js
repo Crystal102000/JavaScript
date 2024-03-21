5fff788e3975d315261e3808fb262d8c791e4e96
Crystal102000 <83111253+Crystal102000@users.noreply.github.com>
2024-03-21 20:13:25 +0000

    varaibles check

---
+++ 1_basics/variables.js
@@ -0,0 +1,14 @@
+const id = 123;
+var username = 'Crystal' //var keyword was used in old days bt scope issues occured and let came into picture 
+let hobby = 'Dance';
+age = 23;
+let location;
+
+username = 'Lancylord';
+hobby = 'gaming';
+age = 23;
+
+console.log('id');
+
+
+console.table([username, hobby, age, location]);
