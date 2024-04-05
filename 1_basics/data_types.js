91f5c44955c17e4d2b0b8696466f4e918163d6a1
Crystal102000 <83111253+Crystal102000@users.noreply.github.com>
2024-04-05 20:40:49 +0000

    data types

---
+++ 1_basics/data_types.js
@@ -0,0 +1,25 @@
+"use strict"; //treats js code as a new version
+
+// alert(3+3); // we are using node js and not browser
+
+let name = "Crystal";
+let age = 22;
+let subscribed = true;
+let state;
+let temp = null;
+
+// console.log(typeof "Crystal"); // output: string
+// console.log(typeof "name"); // output : string
+
+// console.log(typeof 22); // output: number
+// console.log(typeof age); // output: number
+
+// console.log(typeof true); // output: boolean
+// console.log(typeof subscribed); // output: boolean
+
+// console.log(typeof state); // output: undefined
+
+// console.log(typeof temp); // output: object
+
+// console.log(typeof null); // output: object
+// console.log(typeof undefined); //output: undefined
