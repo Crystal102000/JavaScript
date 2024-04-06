0de0953237a60bb0c62d9dc87042c909ff35c85f
Crystal102000 <83111253+Crystal102000@users.noreply.github.com>
2024-04-06 15:46:21 +0000

    convert datatypes

---
+++ 1_basics/03_conversion_operation.js
@@ -0,0 +1,33 @@
+let check = 32;
+check = null;
+
+let valueCheck = Number(check);
+
+// console.log(typeof check);
+
+// console.log(typeof valueCheck);
+// console.log(valueCheck);
+
+// 33 => 33
+// '33string' => NaN
+// true => 1
+//false=> 0
+//null => 0
+
+let isLoggedIn = undefined;
+let booleanCheck = Boolean(isLoggedIn);
+// console.log(typeof booleanCheck);
+// console.log(booleanCheck);
+
+// 1=> true 0=>false
+// undefined, null => false
+//"" => false
+//"crys"=> true;
+
+//number to string conversion
+
+let arreNum = null
+let convertNum = String(arreNum);
+console.log(convertNum);
+console.log(typeof convertNum);
+
